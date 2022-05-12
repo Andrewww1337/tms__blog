7bde867ed3f7d409c0d22467c260366e9ff86b3a
Andrewww1337 <93862187+Andrewww1337@users.noreply.github.com>
2022-05-12 12:50:53 +0400

    lessin_38

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,171 @@
-import React from 'react';
-import './components/input/input.css'
+import React from "react";
+import "./components/input/input.css";
 
-import './App.css';
-import { NameForm } from './components/input';
-import { NameTextarea } from './components/textarea';
-import { PostCard } from './components/postCard';
-import Space from './components/postCard/img/Rectangle 39.png'
-import SpaceMedium from './components/postCard/img/Rectangle 39  med.png'
-import SpaceSmall from './components/postCard/img/Rectangle 39 small.png'
-import { PostList } from './components/postList';
+import "./App.css";
+import { NameForm } from "./components/input";
+import { NameTextarea } from "./components/textarea";
+import { PostCard } from "./components/postCard";
+import Space from "./components/postCard/img/Rectangle 39.png";
+import SpaceMedium from "./components/postCard/img/Rectangle 39  med.png";
+import SpaceSmall from "./components/postCard/img/Rectangle 39 small.png";
+import { PostList } from "./components/postList";
+import { Login } from "./components/login";
 
-const postsFromSer= [
+export const ThemContext = React.createContext("light");
+
+const postsFromSer = [
   {
-      size:"big",
-      cardName:"Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
-      text:"Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
-      img:"https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
-      time:"29 April,2022",
-
-
+    size: "big",
+    cardName:
+      "1Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
   },
   {
-      size:"big",
-      cardName:"Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
-      text:"Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
-      img:"https://276709.selcdn.ru/proektoria/professions/2020/04/29/c22abfa379f38b5b0411bc11fa9bf92f/2020-04-29_12-33-02.jpg",
-      time:"29 April,2022",
+    size: "medium",
+    cardName:
+      "2Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "medium",
+    cardName:
+      "3Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "medium",
+    cardName:
+      "4Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "medium",
+    cardName:
+      "5Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
 
-
-  },{
-      size:"big",
-      cardName:"Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
-      text:"Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.",
-      img:"https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
-      time:"29 April,2022",
-
-
-  }
- 
-]
-
-
+  {
+    size: "small",
+    cardName:
+      "6Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "small",
+    cardName:
+      "7Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "small",
+    cardName:
+      "8Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "small",
+    cardName:
+      "9Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "small",
+    cardName:
+      "10Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+  {
+    size: "small",
+    cardName:
+      "11Astronauts prep for new solar arrays on nearly seven-hour spacewalk",
+    text: "",
+    img: "https://naukatehnika.com/files/vse_zhurnaly/2019/12.19/img-6.jpg",
+    time: "29 April,2022",
+  },
+];
+const colour = "dark";
 function App() {
   return (
-    <div className="App">
+    <ThemContext.Provider value={colour}>
+      <div className="App">
+        <NameForm
+          inputType="password"
+          placeholder=""
+          label="Password"
+          disabled={false}
+        />
 
-<NameForm inputType="password" placeholder='' label='Password' disabled={false}/>
+        <NameForm
+          inputType="email"
+          placeholder=""
+          label="Email"
+          disabled={true}
+        />
 
-<NameForm inputType="email" placeholder='' label='Email' disabled={true}/>
+        <NameForm
+          inputType="text"
+          placeholder=""
+          label="Text"
+          disabled={false}
+        />
 
-<NameForm inputType="text" placeholder='' label='Text' disabled={false}/>
+        <NameTextarea
+          rows={10}
+          cols={22}
+          id="textarea"
+          placeholder="It was a dark and stormy night..."
+          label="Text Area"
+        />
 
-<NameTextarea rows={10} cols={22} id="textarea" placeholder='It was a dark and stormy night...' label='Text Area'/>
+        <PostCard
+          size="big"
+          cardName="Astronauts prep for new solar arrays on nearly seven-hour spacewalk"
+          text="Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight."
+          img={Space}
+          time="April 20,2021"
+        />
 
-<PostCard size='big' 
-cardName='Astronauts prep for new solar arrays on nearly seven-hour spacewalk' 
-text='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.' 
-img={Space} time='April 20,2021'/>
+        <PostCard
+          size="medium"
+          cardName="Astronauts prep for new solar arrays on nearly seven-hour spacewalk"
+          text=""
+          img={SpaceMedium}
+          time="April 20,2021"
+        />
 
-<PostCard size='medium'
-cardName='Astronauts prep for new solar arrays on nearly seven-hour spacewalk' 
-text='' 
-img={SpaceMedium} time='April 20,2021'/>
+        <PostCard
+          size="small"
+          cardName="Astronauts prep for new solar arrays on nearly seven-hour spacewalk"
+          text=""
+          img={SpaceSmall}
+          time="April 20,2021"
+        />
 
-<PostCard size='small'
-cardName='Astronauts prep for new solar arrays on nearly seven-hour spacewalk' 
-text='' 
-img={SpaceSmall} time='April 20,2021'/>
-
-
-<PostList PostsFrom={postsFromSer}/>
-    </div>
+        <PostList PostsFrom={postsFromSer} />
+      </div>
+      <Login></Login>
+    </ThemContext.Provider>
   );
 }
 
--- src/components/input/form.tsx
+++ src/components/input/form.tsx
@@ -1,49 +1,65 @@
-
-import React, {useState} from 'react';
-import './input.css'
+import React, { useEffect, useRef, useState } from "react";
+import "./input.css";
 
 type InputProps = {
-  inputType:string
-  placeholder:string
-  disabled:boolean
-  label:string
-
+  inputType: string;
+  placeholder: string;
+  disabled: boolean;
+  label: string;
+  inputRef?: any;
+  onChange?: (event: { target: { value: any } }) => void;
 };
 
-export const NameForm = ({inputType ,label, disabled, placeholder}: InputProps) => {
+export const NameForm = ({
+  inputType,
+  label,
+  disabled,
+  inputRef,
+  onChange,
 
-
-
-    const [inputValue, setInputValue] = useState('')
-    const [error, setError] = useState('')
-    const handleChange = (event: { target: { value: any; }; }) => {
-      setInputValue(event.target.value)
-      if (
-        (event.target.value).length < 5 
-      && inputType==="password"){
-        setError('error')
-      }else{
-        setError('')
-      }
-    }
-  
-    
-    
-    const handleSubmit = (event: { preventDefault: () => any; }) => {
-      
-      alert('Отправленная строка: ' + inputValue + error);
-      event.preventDefault();
-     
-    }
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p className='labelText'>{label}</p>
-          <input type={inputType} placeholder={inputType} className={`${inputType} ${error}`} value={inputValue} onChange={handleChange} disabled={disabled}/>
-          <p className={error&&'errorP'}>{error}</p>
-        </label>
-        
-      </form>
-    );
+  placeholder,
+}: InputProps) => {
+  function ttt() {
+    console.log("ttt");
   }
+  /*const inputRef = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+    inputRef.current?.addEventListener("click", ttt);
+  });*/
+
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+  const handleChange = (event: { target: { value: any } }) => {
+    setInputValue(event.target.value);
+    if (event.target.value.length < 5 && inputType === "password") {
+      setError("error");
+    } else {
+      setError("");
+    }
+  };
+
+  const handleSubmit = (event: { preventDefault: () => any }) => {
+    alert("Отправленная строка: " + inputValue + error);
+    console.log(inputRef.current?.value);
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <p className="labelText">{label}</p>
+        <input
+          ref={inputRef}
+          type={inputType}
+          placeholder={placeholder}
+          className={`${inputType} ${error}`}
+          value={inputValue}
+          onChange={handleChange}
+          disabled={disabled}
+        />
+        <p className={error && "errorP"}>{error}</p>
+      </label>
+    </form>
+  );
+};
